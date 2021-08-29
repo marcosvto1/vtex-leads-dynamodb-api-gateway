6f85d7799cb2d5b7141f3e10a03b0c5cad9a7001
Marcos <marcosvto1@gmail.com>
2021-08-29 20:11:02 -0300

    refactor method create lead

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const db = require("./db");
+const uuid = require('uuid')
 const {
     GetItemCommand,
     PutItemCommand,
@@ -32,17 +33,22 @@ const getAllLeads = async () => {
   return response;
 };
 
-const createLead = async (event) => {
+const createOneLead = async (event) => {
   const response = { statusCode : 200 };
 
   try {
-
-  } catch (e) {
-    console.log(e);
     const body = JSON.parse(event.body);
+
+    const leadNew = {
+      ...body,
+      leadId: uuid.v4(),
+      type: 'prospect',
+      prospectAt: new Date().toString()
+    }
+
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall(body || {}),
+      Item: marshall(leadNew || {}),
     };
 
     const createResult = await db.send(new PutItemCommand(params));
@@ -51,11 +57,19 @@ const createLead = async (event) => {
       message: 'Succesfully created lead',
       createResult
     })
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+        message: "Failed to create post.",
+        errorMsg: e.message,
+        errorStack: e.stack,
+    });   
   }
 }
 
 
 module.exports = {
   getAllLeads,
-  createLead
+  createOneLead
 }
