da0272e91fbc7533c84c98ccaf57aa765aa8d566
Marcos <marcosvto1@gmail.com>
2021-08-29 19:46:43 -0300

    refactor getAllLeads

--- api.js
+++ api.js
@@ -12,30 +12,25 @@ const getAllLeads = async () => {
   const response = { statusCode: 200 };
 
   try {
-    const { Items } = await db.send(new ScanCommand(
-      {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-      }
-    ))
+      const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
 
-    response.body = JSON.stringify({
-      data: Items.map((item) => unmarshall(item)),
-      Items,
-    });
-
+      response.body = JSON.stringify({
+          message: "Successfully retrieved all leads.",
+          data: Items.map((item) => unmarshall(item)),
+          Items,
+      });
   } catch (e) {
-    console.log(e);
-
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to retrieve leads',
-      errorMsg: e.message,
-      errorStack: e.stack
-    })
-
-    return response;
+      console.error(e);
+      response.statusCode = 500;
+      response.body = JSON.stringify({
+          message: "Failed to retrieve leads.",
+          errorMsg: e.message,
+          errorStack: e.stack,
+      });
   }
-}
+
+  return response;
+};
 
 module.exports = {
   getAllLeads
